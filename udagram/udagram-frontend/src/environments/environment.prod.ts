50b4a095c9ddbab94de83e6f6f7c05a011db277c
omar214 <omarmohamed2322000@gmail.com>
2022-09-05 23:09:44 +0200

    change api URL

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+	production: true,
+	appName: 'Udagram',
+	apiHost:
+		'http://udagram-api-dev.eba-pjccgjs3.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+	production: false,
+	appName: 'Udagram',
+	apiHost:
+		'http://udagram-api-dev.eba-pjccgjs3.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
