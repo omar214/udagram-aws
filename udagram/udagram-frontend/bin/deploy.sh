de21fe57de667794c3a99c5a97b13a7583ded2a3
Omar Mohamed <omarmohamed2322000@gmail.com>
2022-09-06 02:33:47 +0200

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp ./www s3://udagram-front/ --recursive --acl public-read 
-aws s3 cp ./www/index.html s3://udagram-front/  --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" 
+aws s3 cp ./www s3://udagram-front/ --recursive  
+aws s3 cp ./www/index.html s3://udagram-front/  --cache-control="max-age=0, no-cache, no-store, must-revalidate" 
